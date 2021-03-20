bdd030ba2136017a01b707a3fa5e2a64a50a76b6
Geoff Boeing <boeing@usc.edu>
2021-03-19 22:40:10 -0700

    update linter and formatter

--- lint_format.sh
+++ lint_format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-nbqa isort notebooks/*.ipynb --nbqa-mutate
-nbqa black notebooks/*.ipynb --nbqa-mutate
-nbqa flake8 notebooks/*.ipynb --max-line-length 100
-jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace notebooks/*.ipynb
+nbqa isort ./notebooks/*.ipynb --nbqa-mutate --line-length 100 --sl
+nbqa black ./notebooks/*.ipynb --nbqa-mutate --line-length 100
+nbqa flake8 ./notebooks/*.ipynb --max-line-length 100
+jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ./notebooks/*.ipynb
