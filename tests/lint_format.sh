0e7aa44fdfbcd20ece3b8e88fad25d65dd7dbb1a
Geoff Boeing <boeing@usc.edu>
2023-06-28 15:25:55 -0700

    update linter

--- tests/lint_format.sh
+++ tests/lint_format.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
-nbqa isort ./notebooks/*.ipynb --line-length 100 --sl
-nbqa black ./notebooks/*.ipynb --line-length 100
-nbqa flake8 ./notebooks/*.ipynb --max-line-length 100
+pre-commit run --all-files
 jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ./notebooks/*.ipynb
